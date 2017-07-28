db07a43e8546549404f0085be42eb12e6086adb2
Max <maxgrasbeck@gmail.com>
2017-07-28 11:29:32 +0300

    Make external_id unique
    Make the external_id unique so we prevent duplicates.

--- stories/models/story.py
+++ stories/models/story.py
@@ -22,6 +22,8 @@ class Story(TranslatableModel):
 
     external_id = models.CharField(
         max_length=255,
+        unique=True,
+        null=True,
         blank=True,
     )
     location = models.GeometryField(
