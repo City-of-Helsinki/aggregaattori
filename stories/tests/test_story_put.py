7634e11cee4dde0d4fea7bca38d38daa355c9144
Max <maxgrasbeck@gmail.com>
2017-08-11 17:40:51 +0300

    Add test for story put

---
+++ stories/tests/test_story_put.py
@@ -0,0 +1,77 @@
+import pytest
+from rest_framework.test import APIClient
+
+
+class TestStoryPut:
+    client = APIClient()
+    url = 'http://testserver/v1/story/test:1234/'
+
+    story = {
+        "location": {
+            "type": "Point",
+            "coordinates": [
+                25.000000,
+                60.170833
+            ]
+        },
+        "keywords": [
+            "yso:p8471"
+        ],
+        "ocd_id": "test",
+        "location_id": "test:4321",
+        "translations": {
+            "en": {
+                "title":"English test title",
+                "short_text": "Short text.",
+                "text": "English test text.",
+                "url": "http://example.org/en"
+            }
+        }
+    }
+
+    @pytest.mark.django_db
+    def test__put_create(self):
+
+        response = self.client.put(
+            self.url,
+            self.story,
+            format='json',
+        )
+
+        assert response.status_code == 201
+
+    @pytest.mark.django_db
+    def test__put_modify(self):
+        response = self.client.put(
+            self.url,
+            self.story,
+            format='json',
+        )
+
+        modified_story = self.story.copy()
+        modified_story['location_id'] = 'test:2345'
+
+        response = self.client.put(
+            self.url,
+            modified_story,
+            format='json',
+        )
+
+        assert response.status_code == 200
+
+    @pytest.mark.django_db
+    def test__put_and_get_story(self):
+        response = self.client.put(
+            self.url,
+            self.story,
+            format='json',
+        )
+
+        response = self.client.get(self.url)
+        response_story = response.json()
+
+        assert response_story['properties']['external_id'] == 'test:1234'
+        assert response_story['properties']['location_id'] == 'test:4321'
+        assert response_story['properties']['keywords'] == ["yso:p8471"]
+
+        assert response.status_code == 200
