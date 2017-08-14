24d463bb8032257008246341d77056a23f6100b3
Max <maxgrasbeck@gmail.com>
2017-08-14 12:44:06 +0300

    Add gis admin

--- stories/admin.py
+++ stories/admin.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.contrib.gis.admin import OSMGeoAdmin
 from parler.admin import TranslatableAdmin
 from stories.models import Story
-
+from munigeo.models import AdministrativeDivisionGeometry
 
 class StoryAdmin(OSMGeoAdmin, TranslatableAdmin):
     pass
 
+class AdministrativeDivisionGeometryAdmin(OSMGeoAdmin):
+    pass
 
 admin.site.register(Story, StoryAdmin)
+admin.site.register(AdministrativeDivisionGeometry, AdministrativeDivisionGeometryAdmin)
