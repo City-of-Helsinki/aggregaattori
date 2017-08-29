aa62456a8338394438dca80658fd793e5e8e14b5
Max <maxgrasbeck@gmail.com>
2017-08-29 15:52:14 +0300

    Make keywords conform to Activity Streams format

--- stories/models/keyword.py
+++ stories/models/keyword.py
@@ -1,12 +1,22 @@
+from parler.models import TranslatableModel, TranslatedFields
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
-class Keyword(models.Model):
-    yso = models.CharField(
+class Keyword(TranslatableModel):
+    translations = TranslatedFields(
+        name=models.CharField(
+            verbose_name=_('Name'),
+            max_length=255,
+            blank=True,
+        ),
+    )
+
+    external_id = models.CharField(
         max_length=255,
         unique=True,
     )
+
 
     class Meta:
         verbose_name = _('Keyword')
